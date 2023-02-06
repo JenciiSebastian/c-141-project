85ba2b435318af2492c10a6d007449886b3069c3
JenciiSebastian <85684508+JenciiSebastian@users.noreply.github.com>
2023-02-06 18:02:28 +0530

    Add files via upload

---
+++ new_scraper.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+import csv
+
+# NASA Exoplanet URL
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+
+# Webdriver
+browser = webdriver.Chrome("C:/Users/Jencii/Downloads/chromedriver_win32/chromedriver.exe")
+browser.get(START_URL)
+
+time.sleep(10)
+
+#planets_data = []
+
+# Define Exoplanet Data Scrapping Method
+def scrape():
+    headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+    planets_data = []
+    for i in range(0,10):
+        print(f'Scrapping page {i+1} ...' )
+        
+        # BeautifulSoup Object     
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+
+        # Loop to find element using XPATH
+        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+
+            li_tags = ul_tag.find_all("li")
+           
+            temp_list = []
+
+            for index, li_tag in enumerate(li_tags):
+
+                if index == 0:                   
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+
+            planets_data.append(temp_list)
+        print(planets_data)    
+
+        # Find all elements on the page and click to move to the next page
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+        with open("batch1.csv","w")as  f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(headers)
+            csvwriter.writerows(planets_data)
+
+# Calling Method    
+scrape()
+
+# Define Header
+#headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+
+# Define pandas DataFrame   
+#planet_df_1 = pd.DataFrame(planets_data, columns=headers)
+
+# Convert to CSV
+#planet_df_1.to_csv('scraped_data.csv',index=True, index_label="id")
